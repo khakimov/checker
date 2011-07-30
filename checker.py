7ae54c04c3531986ea0386511c624b733bdf0982
Pupsor <pupsor@gmail.com>
2011-07-29 18:40:18 -0700

    checker

--- checker.py
+++ checker.py
@@ -1,56 +1,50 @@
-import threading
-import Queue
-import time
-import urllib2
-from urllib2 import Request, urlopen, URLError, HTTPError
+from urlparse import urlparse
+from threading import Thread
+import httplib, sys
+from Queue import Queue
 
-# The worker thread gets jobs off the queue.  When the queue is empty, it
-# assumes there will be no more work and exits.
-# (Realistically workers will run until terminated.)
-def worker ():
-    print 'Running worker'
-    time.sleep(0.1)
+concurrent = 10
+
+def doWork():
     while True:
-        try:
-            arg = q.get(block=False)
-        except Queue.Empty:
-            print 'Worker', threading.currentThread(),
-            print 'queue empty'
-            break
-        else:
-            print 'Worker', threading.currentThread(),
-            print 'running with argument', arg
-            url = urllib2.Request(arg)
-            try:
-                response = urlopen(url, timeout=3)
-            except:
-                print 'We failed to reach a server.'
-            else:
-                try:
-                    text = response.read()
-                    index = text.find("href")
-                    if (index > 0):
-                        print "yahooo, got one: " + arg
-                        w = open("found", "a")
-                        w.write(arg)
-                        w.close()
-                except:
-                    print "can't read from response"
-                        
-# Create queue
-q = Queue.Queue()
+        url=q.get()
+        status,url=getStatus(url)
+        doSomethingWithResult(status,url)
+        q.task_done()
 
-# Start a pool of 5 workers
-for i in range(5):
-    t = threading.Thread(target=worker, name='worker %i' % (i+1))
+def getStatus(ourl):
+    try:
+        url = urlparse(ourl)
+        conn = httplib.HTTPConnection(url.netloc)   
+        conn.request("GET", url.path)
+        res = conn.getresponse()
+        server = res.getheader('server')
+        data = res.read()
+        print res.status, server, ourl
+        index = data.find("href")
+        if (index > 0):
+            print "yahooo, got one: " + ourl
+            w = open("found", "w")
+            w.write(ourl)
+            w.close()
+            
+        return res.status, ourl
+    except:
+        return "error", ourl
+
+def doSomethingWithResult(status, url):
+    print status, url
+
+q=Queue(concurrent*2)
+for i in range(concurrent):
+    t=Thread(target=doWork)
+    t.daemon=True
     t.start()
-
-# Begin adding work to the queue
-for line in open('urls'):
-    f = line.split(',')[-1]
-    host = "http://" + f[:-1] + "/request"
-    q.put(host)
-    
-# Give threads time to run
-print 'Main thread sleeping'
-time.sleep(5)
+try:
+    for line in open('top-1m.csv'):
+        f = line.split(',')[-1]
+        url = "http://" + f[:-1] + "/req"
+        q.put(url.strip())
+    q.join()
+except KeyboardInterrupt:
+    sys.exit(1)
